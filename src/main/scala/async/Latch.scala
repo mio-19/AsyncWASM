e79469a17d3353c47e6c1d8d0488fcd5c4776d35
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-01-28 08:40:55 -0800

    up

---
+++ src/main/scala/async/Latch.scala
@@ -0,0 +1,63 @@
+package async
+
+import chisel3._
+
+// Fancy dual rail half latch
+class Latch[T <: Data](A: T) extends Module {
+  val io = IO(new Bundle {
+    val input = Input(Dual(A))
+    val inputACK = Output(Bool())
+
+    val output = Output(Dual(A))
+    val outputACK = Input(Bool())
+  })
+
+  val inputACK = RegInit(false.B)
+  io.inputACK := inputACK
+
+  private val width = A.getWidth
+  val inputACKs = Wire(Vec(width, Bool()))
+  val latch0s = (0 until width).map(i => {
+    val latch0 = Module(new Latch0)
+    latch0.io.input0 := io.input.zeros.asUInt.apply(i)
+    latch0.io.input1 := io.input.ones.asUInt.apply(i)
+    latch0.io.inputACK := inputACKs(i)
+    latch0.io.output0 := io.output.zeros.asUInt.apply(i)
+    latch0.io.output1 := io.output.ones.asUInt.apply(i)
+    latch0.io.outputACK := io.outputACK
+  })
+
+  // todo: check me
+  io.inputACK := (0 until width).map(i => inputACKs(i)).reduce(_ && _)
+}
+
+class Latch0 extends Module {
+  val io = IO(new Bundle {
+    val input0 = Input(Bool())
+    val input1 = Input(Bool())
+    val inputACK = Output(Bool())
+
+    val output0 = Output(Bool())
+    val output1 = Output(Bool())
+    val outputACK = Input(Bool())
+  })
+
+  val input0 = RegInit(false.B)
+  input0 := io.input0
+  val input1 = RegInit(false.B)
+  input1 := io.input1
+  val outputACK_not = RegInit(false.B)
+  outputACK_not := !io.outputACK
+
+  val c0 = Module(new C)
+  c0.io.value1 := outputACK_not
+  c0.io.value2 := input0
+  c0.io.output := io.output0
+
+  val c1 = Module(new C)
+  c0.io.value1 := outputACK_not
+  c0.io.value2 := input1
+  c0.io.output := io.output1
+
+  io.inputACK := io.output0 || io.output1
+}
