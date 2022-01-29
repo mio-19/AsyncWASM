f358e5cebfed013f302bd605b6c74bc4148ce4e4
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-01-29 05:23:05 -0800

    broken

--- src/main/scala/async/UartTx.scala
+++ src/main/scala/async/UartTx.scala
@@ -14,49 +14,13 @@ class UartTx(baudDivisor: Int) extends Module {
     val txd = Output(Bool())
   })
 
-  val l0 = Module(new Lat(UInt(8.W)))
-  l0.io.input <> io.value
-  val value = l0.io.output
-
   val u0 = Module(new UartTxSync(baudDivisor))
-  io.txd := u0.io.txd
-  val busy = u0.io.busy
-  val start = RegInit(Bool(), false.B)
-  u0.io.start := start
-  u0.io.data := value.unsafeExtract
+  io.txd := u0.txd
+  val s0 = Module(new SyncToAsync(UInt(8.W)))
+  s0.reset := reset
+  u0.io.data := s0.io.data
+  u0.io.start := s0.io.enable
+  s0.io.busy := u0.io.busy
 
-  val enStart = Wire(Bool())
-  when(start) {
-    start := false.B
-  }.elsewhen(value.unsafeGotData && enStart && !busy) {
-    start := true.B
-  }
-
-  // State 0 start=F enStart=T valueACK=F
-  // State 1 start=T enStart=T valueACK=F gotData
-  // State 1.5 start=T enStart=F valueACK=F gotData
-  // State 2 start=F enStart=F valueACK=F gotData
-  // State 2.5 start=F enStart=F valueACK=T gotData
-  // State 3 start=F enStart=F valueACK=T isRTZ
-  // State 3.5 start=F enStart=T valueACK=F isRTZ
-  when(reset.asBool) {
-    enStart := true.B // State 0
-  }.elsewhen(start) { // State 1
-    enStart := false.B
-  }.elsewhen(value.unsafeIsRTZ) { // State 3
-    enStart := true.B
-  }.otherwise {
-    enStart := enStart
-  }
-  val valueACK = Wire(Bool())
-  value.ack := valueACK
-  when(reset.asBool) {
-    valueACK := false.B
-  }.elsewhen(!start && !enStart && !valueACK) { // State 2
-    valueACK := true.B
-  }.elsewhen(value.unsafeIsRTZ) { // State 3
-    valueACK := false.B
-  }.otherwise {
-    valueACK := valueACK
-  }
+  s0.async <> io.value
 }
--- src/main/scala/sync/UartTxSync.scala
+++ src/main/scala/sync/UartTxSync.scala
@@ -2,7 +2,6 @@ package sync
 
 import chisel3._
 import chisel3.util.MixedVecInit
-import chisel3.experimental.BundleLiterals._
 
 // based on https://github.com/pConst/basic_verilog/blob/a1608c2326c057dbe4831e7e5e68ee2c1a769f9b/uart_tx.sv
 class UartTxSync(baudDivisor: Int) extends Module {
@@ -18,16 +17,14 @@ class UartTxSync(baudDivisor: Int) extends Module {
     val txd = Output(Bool())
   })
 
-  val ShifterTxd = new Bundle {
-    val shifter = UInt(10.W)
-    val txd = Bool()
-  }
-
-  val shifterTxd = RegInit(ShifterTxd, ShifterTxd.Lit(_.shifter -> 0.U, _.txd -> true.B))
-
-  val shifter = shifterTxd.shifter
-  val txd = shifterTxd.txd
+  val shifter = RegInit(UInt(10.W), 0.U)
+  val txd = RegInit(Bool(), true.B)
   io.txd := txd
+
+  val shifterTxd = Wire(new ShifterTxd)
+  shifterTxd.shifter := shifter
+  shifterTxd.txd := txd
+
   val sampleCntr = RegInit(UInt(16.W), (baudDivisor - 1).U)
 
   when(sampleCntr === 0.U) {
@@ -52,10 +49,15 @@ class UartTxSync(baudDivisor: Int) extends Module {
   }
     .otherwise {
       when(doSample) {
-        shifterTxd := (shifterTxd.asUInt >> 1).asTypeOf(ShifterTxd);
+        shifterTxd := (shifterTxd.asUInt >> 1).asTypeOf(new ShifterTxd);
         when(VecInit(shifter.asBools.slice(1, 10)).asUInt === 0.U) {
           busy := false.B
         }
       }
     }
+}
+
+class ShifterTxd extends Bundle {
+  val shifter = UInt(10.W)
+  val txd = Bool()
 }
