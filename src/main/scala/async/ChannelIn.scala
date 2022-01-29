16e297577ee111e903a90fd2b76024b9558f20df
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-01-29 00:18:48 -0800

    up

--- src/main/scala/async/ChannelIn.scala
+++ src/main/scala/async/ChannelIn.scala
@@ -5,6 +5,14 @@ import chisel3._
 class ChannelIn[T <: Data](A: T) extends Bundle {
   val dual = Input(Dual(A))
   val ack = Output(Bool())
+
+  def unsafeIsCleared: Bool = dual.unsafeIsCleared
+
+  def unsafeIsValid: Bool = dual.unsafeIsValid
+
+  def unsafeExtract: T = dual.unsafeExtract
+
+  def unsafeIsRTZ: Bool = dual.unsafeIsCleared && ack
 }
 
 object ChannelIn {
--- src/main/scala/async/UartTx.scala
+++ src/main/scala/async/UartTx.scala
@@ -16,25 +16,24 @@ class UartTx(baudDivisor: Int) extends Module {
 
   val u0 = Module(new UartTxSync(baudDivisor))
   io.txd := u0.io.txd
-  u0.io.data := io.value.dual.unsafeExtract
+  u0.io.data := io.value.unsafeExtract
   val busy = u0.io.busy
   val start = RegInit(Bool(), false.B)
   u0.io.start := start
 
   when(start) {
     start := false.B
-  }.elsewhen(io.value.dual.unsafeIsValid && !busy && !io.value.ack) {
+  }.elsewhen(io.value.unsafeIsValid && !busy && !io.value.ack) {
     start := true.B
   }
 
   val valueACK = Wire(Bool())
   io.value.ack := valueACK
-  val RTZ = io.value.dual.unsafeIsCleared && io.value.ack
   when(reset.asBool) {
     valueACK := false.B
   }.elsewhen(start) {
     valueACK := true.B
-  }.elsewhen(RTZ) {
+  }.elsewhen(io.value.unsafeIsRTZ) {
     valueACK := false.B
   }.otherwise {
     valueACK := valueACK
